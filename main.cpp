cafb38ea7e5e05793b838e6f7eb92c42429a38e3
JaredMel <jareddmelendez1764@gmail.com>
2024-09-22 20:23:09 -0700

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+///******************************************************************************
+//Description: This program will print "Hello Github!"
+//Author: Jared Melendez
+//COMSC 210 Section 5293
+//Date September 22, 2024
+//Status: Done
+///******************************************************************************
+
+#include <iostream>
+using namespace std;
+//the main function
+int main()
+{
+    //Prints message
+    cout << "Hello Github!";
+}
